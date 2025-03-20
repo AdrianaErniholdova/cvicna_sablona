6df5348994481ce8a82da1bc2eeb410d2f8b227c
AdrianaErniholdova <adrianaerniholdova@gmail.com>
2025-03-20 23:21:03 +0100

    aktivita2_zmeny_v_portfolio

--- parts/functions.php
+++ parts/functions.php
@@ -1,5 +1,5 @@
 <?php
-function generateSlides($dir) {
+function generateSlides($dir) { //banner na index.php
     $files = glob($dir . "/*.jpg");
     $json = file_get_contents("data/datas.json");
     $data = json_decode($json, true);
@@ -13,5 +13,30 @@ function generateSlides($dir) {
         echo '</div>';
     }
 }
+
+function preparePortfolio(int $numberOfRows = 2, int $numberOfCols = 4): array{
+    $portfolio = [];
+    $colIndex = 1;
+    for ($i = 1; $i <= $numberOfRows; $i++) {
+        for($j = 1; $j <= $numberOfCols; $j++) {
+            $portfolio[$i][$j] = $colIndex;
+            $colIndex++;
+        }
+    }
+    return $portfolio;
+}
+function finishPortfolio(){
+    $portfolio = preparePortfolio();
+    foreach ($portfolio as $row => $col) {
+        echo '<div class="row">';
+        foreach ($col as $index) {
+            echo '<div class="col-25 portfolio text-white text-center" id="portfolio-'.$index.'">
+                                        Web stránka '.$index.'
+                                                                 </div>';
+        }
+        echo '</div>';
+    }
+}
+
 ?>
 
